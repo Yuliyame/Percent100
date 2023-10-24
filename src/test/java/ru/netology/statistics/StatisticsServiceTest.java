37d6121896fcdfeb7db089df325b64986f5ef5cf
YuliyaYuliya <yr555556666667777777@gmail.com>
2023-10-24 19:35:56 +0300

    Changes StatisticsService4

--- src/test/java/ru/netology/statistics/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.statistics;
 
 import org.junit.jupiter.api.Test;
 
+
 import org.junit.jupiter.api.Assertions;
 
     public class StatisticsServiceTest {
@@ -12,6 +13,17 @@ import org.junit.jupiter.api.Assertions;
 
             long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
             long expected = 12;
+
+            long actual = service.findMax(incomesInBillions);
+
+            Assertions.assertEquals(expected, actual);
+        }
+        @Test
+        void findMaxFromMiddle() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
+            long expected = 30;
 
             long actual = service.findMax(incomesInBillions);
 
